0015dffb472f207317ecd3fc1715d59776518f96
João Antonio A. B. Camilo <joaoantoniobr07@gmail.com>
2026-06-23 21:42:16 -0300

    Remove unused Conquistas import
    Delete the unused Conquistas import from front/src/App.jsx to clean up dead code and avoid linter warnings. The app continues to lazy-load PopulatePage; this change is purely a small cleanup.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -11,7 +11,6 @@ import ProtectedRoute from './components/ProtectedRoute'
 import ResizableSidebarLayout from './components/layout/ResizableSidebarLayout'
 import { AuthProvider } from './contexts/AuthContext'
 import { useAuth } from './hooks/useAuth'
-import Conquistas from './pages/Conquistas'
 const PopulatePage = React.lazy(() => import('./pages/Populate'))
 
 function AppContent() {
